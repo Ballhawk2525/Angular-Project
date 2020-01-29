ad91601eec8816c1b799a3df1103e3658bb79cb7
Mixie Clerard <clerardm@gmail.com>
2020-01-29 00:33:59 -0500

    update favorite/heart

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  favorited = "heart-button-off";
+  favorite() {
+    // let fav:any = document.getElementsByClassName(".heart-button-off");
+    // let favOn:any = document.getElementsByClassName(".heart-button-on");
+    if (this.favorited === "heart-button-off") {
+      // fav.style.display = "none";
+      // fav.style.visibility = "hidden";
+
+      // favOn.style.display = "block";
+      // favOn.style.visibility = "visible";
+
+      this.favorited = "heart-button-on";
+    } else {
+      this.favorited = "heart-button-off";
+    }
   }
 
-  activeClass = 'hide-details'
+  activeClass = "hide-details";
   toggleClass() {
-    if(this.activeClass==="hide-details"){
-        this.activeClass='show-details';
-    }else{
-        this.activeClass='hide-details';
+    if (this.activeClass === "hide-details") {
+      this.activeClass = "show-details";
+    } else {
+      this.activeClass = "hide-details";
     }
   }
 }
