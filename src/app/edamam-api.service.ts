76626c029f53e91c02c8165321d9ef800bd2de5a
Idris Hobdy <idrishobdy@yahoo.com>
2020-01-29 21:32:02 -0500

    changes accepted

--- src/app/edamam-api.service.ts
+++ src/app/edamam-api.service.ts
@@ -8,13 +8,8 @@ import { userCreds } from './titi';
 export class EdamamApiService {
 
   constructor(private http: HttpClient) { }
-<<<<<<< HEAD
-  getData() {
-    return this.http.get(this.apiUrl);
-=======
   getData(userSearch) {
     let apiUrl = `https://api.edamam.com/search?q=${userSearch}&app_id=${userCreds.userID}&app_key=${userCreds.userKey}&from=0&to=3&calories=591-722&health=alcohol-free`;
     return this.http.get(apiUrl);
->>>>>>> 489d1337583093045870dc085e64a3922d356641
   }
 }
